2b2e980c30bf8c3bb3e710537ef7cf689866d2b2
Troy Jones <sntnmjones@gmail.com>
2016-10-15 18:06:59 -0700

    Add files via upload

--- homework_time_tracker.js
+++ homework_time_tracker.js
@@ -7,7 +7,20 @@ var date2s;
 var curDate;
 var curMon;
 var curYear;
+var totalHours;
+var totalMinutes;
+var totalSeconds;
 var t;
+var timeStarted = false;
+var divName;
+var divNum = 0;
+var courseDiv;
+var assignDiv;
+var timeStartDiv;
+var durationDiv;
+var timeStopDiv;
+
+document.cookie = "username=John";
 
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
@@ -28,42 +41,82 @@ function createDurationDiv() {
     document.getElementById('container').appendChild(div1);
 }
 
+// When Time Start is pressed
 function timeStart() {
-    var today = new Date();
-    date1h = today.getHours();
-    date1m = today.getMinutes();
-    date1s = today.getSeconds();
-    date1m = checkTime(date1m);
-    date1s = checkTime(date1s);
-    curMon = today.getUTCMonth();
-    curMon = curMon + 1;
-    curDate = today.getUTCDate();
-    curYear = today.getUTCFullYear();
+    if (!timeStarted) {
+        divNum++;
 
-    createDurationDiv();
-    document.getElementById('time1').innerHTML =curMon + "/" + curDate + "/" + curYear + "  " + date1h + ":" + date1m + ":" + date1s;
+        var today = new Date();
+        date1h = today.getHours();
+        date1m = today.getMinutes();
+        date1s = today.getSeconds();
+        date1m = checkTime(date1m);
+        date1s = checkTime(date1s);
+        curMon = today.getUTCMonth();
+        curMon = curMon + 1;
+        curDate = today.getUTCDate();
+        curYear = today.getUTCFullYear();
 
-    totalTime();
+        createDurationDiv();
+        timeStartDiv = curMon + "/" + curDate + "/" + curYear + "  " + date1h + ":" + date1m + ":" + date1s;
+        document.getElementById('time1').innerHTML = timeStartDiv;
+        totalTime();
+        timeStarted = true;
+    }
+
 }
 
+// when Time Stop is pressed
 function timeStop() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
+    if (timeStarted) {
+        createTimeStopDiv();
+        timeStopDiv = curMon + "/" + curDate + "/" + curYear + "  " + date2h + ":" + date2m + ":" + date2s;
+        document.getElementById('time2').innerHTML = timeStopDiv;
+        courseDiv = document.getElementById('course').value;
+        assignDiv = document.getElementById('assign').value;
+        clearTimeout(t);
 
-    var div = document.createElement('div');
-    div.setAttribute('class', 'select');
-    div.setAttribute('id', 'time2');
+        var newContainer = document.createElement('div');
+        divName = 'newDiv' + divNum;
+        newContainer.setAttribute('id', divName);
+        newContainer.setAttribute('class', 'addedDivs');
+        document.getElementById('results').appendChild(newContainer);
 
-    document.getElementById('container').appendChild(div);
+        var div = document.createElement('div');
+        div.setAttribute('class', 'select');
+        div.innerHTML = courseDiv;
+        document.getElementById(divName).appendChild(div);
 
-    document.getElementById('time2').innerHTML = curMon + "/" + curDate + "/" + curYear + "  " + date2h + ":" + date2m + ":" + date2s;
-    clearTimeout(t);
+        var div1 = document.createElement('div');
+        div1.setAttribute('class', 'select');
+        div1.innerHTML = assignDiv; 
+        document.getElementById(divName).appendChild(div1);
+
+        var div2 = document.createElement('div');
+        div2.setAttribute('class', 'select');
+        div2.innerHTML = timeStartDiv; 
+        document.getElementById(divName).appendChild(div2);
+
+        var div3 = document.createElement('div');
+        div3.setAttribute('class', 'select');
+        div3.innerHTML = durationDiv; 
+        document.getElementById(divName).appendChild(div3);
+
+        var div4 = document.createElement('div');
+        div4.setAttribute('class', 'select');
+        div4.innerHTML = timeStopDiv; 
+        document.getElementById(divName).appendChild(div4);
+
+        var temp = document.getElementById('time1').remove();
+        temp = document.getElementById('duration').remove();
+        temp = document.getElementById('time2').remove();
+
+
+        timeStarted = false;
+    }
 }
 
+// starts and displays timer
 function totalTime() {
     var today = new Date();
     date2h = today.getHours();
@@ -71,11 +124,33 @@ function totalTime() {
     date2s = today.getSeconds();
     date2m = checkTime(date2m);
     date2s = checkTime(date2s);
-    var h = date2h - date1h;
-    var m = date2m - date1m;
-    var s = date2s - date1s;
+    totalHours = date2h - date1h;
+    totalMinutes = date2m - date1m;
+    totalSeconds = date2s - date1s;
 
-    document.getElementById('duration').innerHTML =
-    h + " hours " + m + " minutes " + s + " seconds ";
+    durationDiv = totalHours + " hours " + totalMinutes + " minutes " + totalSeconds + " seconds ";
+    document.getElementById('duration').innerHTML = durationDiv;
     t = setTimeout(totalTime, 500);
 }
+
+function createTimeStopDiv () {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'select');
+    div.setAttribute('id', 'time2');
+
+    document.getElementById('container').appendChild(div);
+}
+
+function createDiv (divId, divClass, appendTo) {
+    var divId = divId;
+    var divClass = divClass;
+    var appendTo = appendTo;
+
+    var div = document.createElement('div');
+    if (divClass != 'none') {
+        div.setAttribute('class', 'divClass');
+    }
+    div.setAttribute('id', 'divId');
+    document.getElementById('appendTo').appendChild(div);
+}
+
